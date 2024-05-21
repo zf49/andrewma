de9b4bb5a953f9816129a12370f0f142ee8f42be
zf49 <zwan686@aucklandnui.ac.nz>
2024-05-21 22:25:42 +1200

    test

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <>
       <section className='footerContact'>
         <div className='container'>
-          <div className='send flex'>
+          <div className='send'>
             <div className='text'>
               <h1>Do You Have Questions ?</h1>
               <p>We'll help you to grow your career and growth.</p>
